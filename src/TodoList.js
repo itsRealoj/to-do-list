8df6f2f440bf0bd2ae4380607f0ddcd795626cb7
itsrealoj <stephenojwang1040@gmail.com>
2020-03-28 10:02:28 +0300

    generating every new todo with a list

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,9 +2,24 @@ import React from 'react';
 import TodoForm from './TodoForm';
 
 export default class TodoList extends React.Component {
+    state = {
+        todos: []
+    };
+
+    addTodo = todo => {
+        this.setState({
+            todos: [todo, ...this.state.todos]
+        });
+    };
+
     render() {
         return(
-            <TodoForm/>
+            <div>
+                <TodoForm  onSubmit={this.addTodo} />
+                {this.state.todos.map(todo => (
+                    <div key={todo.id}>{todo.text}</div>
+                ))}
+            </div>
         )
     }
 }
